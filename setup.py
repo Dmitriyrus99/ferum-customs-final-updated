e5b46fbea91a81a3ea316d62900001fed77fe2d4
Dmitriyrus99 <dmitriyrus99@gmail.com>
2025-05-29 12:39:06 +0300

    correct setuptools package list

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ferum',
+    name='ferum_customs',                       # = app_name в hooks.py
     version='1.0.0',
     description='Ferum custom code package',
     author='Dmitriyrus99',
     author_email='Dmitriyrus99@gmail.com',
-    packages=find_packages('ferum'),
-    zip_safe=False,
+
+    # ищем только то, что лежит в каталоге ferum_customs
+    packages=find_packages(where='.', include=['ferum_customs', 'ferum_customs.*']),
     include_package_data=True,
-    install_requires=['setuptools'],
+    zip_safe=False,
+
+    # зависимости вашего приложения
+    install_requires=[
+        'setuptools',
+        # 'frappe>=15.0.0',  # можно добавить, если хотите фиксировать версию
+    ],
 )
+
